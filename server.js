8e699bd0e2a21619ea237cef46b76ddcfa82de07
nudoru <kheavy@nudoru.com>
2015-07-07 10:47:32 -0400

    Related artist search

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var getFromApi = function(endpoint, args) {
     unirest.get('https://api.spotify.com/v1/' + endpoint)
            .qs(args)
            .end(function(response) {
-               console.log('end')
+                console.log('getFromApi, ending');
                emitter.emit('end', response.body);
             });
     return emitter;
@@ -17,9 +17,9 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/search/:name', function(req, res) {
+    console.log('Search for',req.params.name);
 
-    console.log('search for',req.params.name);
-
+    // search?q=<name>&limit=1&type=artist
     var searchReq = getFromApi('search', {
         q: req.params.name,
         limit: 1,
@@ -27,13 +27,38 @@ app.get('/search/:name', function(req, res) {
     });
 
     searchReq.on('end', function(item) {
-        var artist = item.artists.items[0];
-        res.json(artist);
+        console.log('Search request end...');
+        console.log('Requesting related ...');
+
+        var artist = item.artists.items[0],
+          artistID, relatedReq;
+
+        if(!artist) {
+          console.log('Artist not found!');
+          res.sendStatus(404);
+          return
+        }
+
+        artistID = artist['id'];
+        relatedReq = getFromApi('artists/'+artistID+'/related-artists', {});
+
+        // GET https://api.spotify.com/v1/artists/{id}/related-artists
+        relatedReq.on('end', function(item) {
+            console.log('Related request end');
+            artist.related = item.artists;
+            res.json(artist);
+        });
+
+        relatedReq.on('error', function() {
+           console.log('RELATED ERROR - 404');
+           res.sendStatus(404);
+        });
+
     });
 
     searchReq.on('error', function() {
-       console.log('error - 404');
-        res.sendStatus(404);
+       console.log('SEARCH ERROR 404');
+       res.sendStatus(404);
     });
 });
 
